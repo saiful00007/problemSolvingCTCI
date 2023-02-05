72e126d7d4d12a0dcad0a0be8696b91a4ab4d44e
saiful00007 <67178637+saiful00007@users.noreply.github.com>
2023-02-06 00:24:40 +0600

    second commit

--- problemSolving2/permutationString.go
+++ problemSolving2/permutationString.go
@@ -5,24 +5,6 @@ import (
 	"sort"
 )
 
-// func Permutaion(a, b string) bool {
-// 	// var1 := make([]string,10)
-// 	// var2 := make([]string,10)
-
-// 	var var1,var2 string
-// 	if len(var1) != len(var2) {
-// 		return false
-// 	}
-// 	sort.SliceStable(var1, func(i, j int) bool {
-// 		return var1[i] < var1[j]
-// 	})
-// 	sort.SliceStable(var2, func(i, j int) bool {
-// 		return var2[i] < var2[j]
-// 	})
-// 	return string(var1) == string(var2)
-
-// }
-
 func stringsToRune(str string) []rune {
 	mySlice := []rune{}
 	for _, char := range str {
@@ -53,61 +35,20 @@ func main() {
 	fmt.Println(Permutaion("saiful", "fulsai"))
 }
 
-// func stringsToRunes(str string) []rune {
-// 	runeArray := []rune{}
-// 	for _, char := range str {
-// 		runeArray = append(runeArray, rune(char))
-// 	}
-// 	return runeArray
-// }
+// func Permutaion(a, b string) bool {
+// 	// var1 := make([]string,10)
+// 	// var2 := make([]string,10)
 
-// func Permutation(a, b string) bool {
-// 	if len(a) != len(b) {
+// 	var var1,var2 string
+// 	if len(var1) != len(var2) {
 // 		return false
 // 	}
-// 	convertStrings1 := stringsToRunes(a)
-// 	convertStrings2 := stringsToRunes(b)
-
-// 	sort.SliceStable(convertStrings1, func(i, j int) bool {
-// 		return convertStrings1[i] < convertStrings1[j]
+// 	sort.SliceStable(var1, func(i, j int) bool {
+// 		return var1[i] < var1[j]
 // 	})
-// 	sort.SliceStable(convertStrings2, func(i, j int) bool {
-// 		return convertStrings2[i] < convertStrings2[j]
+// 	sort.SliceStable(var2, func(i, j int) bool {
+// 		return var2[i] < var2[j]
 // 	})
-// 	return string(convertStrings1) == string(convertStrings2)
+// 	return string(var1) == string(var2)
 
 // }
-
-// func main() {
-// 	fmt.Println(Permutation("abcd", "bcda"))
-// }
-
-// // func stringsToRunes(str string) []rune {
-// // 	r := []rune{}
-// // 	for _, v := range str {
-// // 		r = append(r, rune(v))
-// // 	}
-// // 	return r
-// // }
-
-// // func Permutation(a, b string) bool {
-// // 	if len(a) != len(b) {
-// // 		return false
-// // 	}
-// // 	c := stringsToRunes(a)
-// // 	d := stringsToRunes(b)
-// // 	sort.SliceStable(c, func(i, j int) bool {
-// // 		return c[i] < c[j]
-// // 	})
-// // 	sort.SliceStable(d, func(i, j int) bool {
-// // 		return d[i] < d[j]
-// // 	})
-// // 	if string(c) != string(d) {
-// // 		return false
-// // 	}
-// // 	return true
-// // }
-
-// // func main() {
-// // 	fmt.Println(Permutation("abcd", "ababcd"))
-// // }
