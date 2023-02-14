c6286745ae0c08a1f03fb737013500e79c25a8d2
saiful00007 <67178637+saiful00007@users.noreply.github.com>
2023-02-14 08:06:32 +0600

    scnd commit

--- problemSolving3/urlFy.go
+++ problemSolving3/urlFy.go
@@ -1,3 +1,16 @@
+// Write a method to replace all spaces in a string with '%20'. You may assume that the string
+// has sufficient space at the end to hold the additional characters, and that you are given the "true"
+// length of the string. (Note: If implementing in Java, please use a character array so that you can
+// perform this operation in place.)
+// EXAMPLE
+// Input: "Mr 3ohn Smit h 13
+// Output: "Mr%203ohn%20Smith"
+
+//declare a function which has 2 parameter,one is string another is for length and return type is string
+//I have to count how many space are there in the given string
+//I create a array which contain the string value as rune type,use append func for that
+//main logic is newLength will be the actualLength+space*2,when we find a space we replace it by %20 for that we shoud go for a loop which is in decrease order
+
 package main
 
 import (
